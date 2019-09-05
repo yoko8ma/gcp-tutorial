a7f54fe52e746211ea09dbf277cf4329ebfc55a4
Hideyuki Yokoyama <yokoyama@cobalt.local>
2019-09-05 22:44:07 +0900

    feat: logging

--- appengine/standard_python37/hello_world/main.py
+++ appengine/standard_python37/hello_world/main.py
@@ -1,9 +1,17 @@
 import responder
 from sklearn.datasets import load_iris
+from google.cloud.logging import Client
+
+client = Client.from_service_account_json("./service_account.json")
+client.setup_logging()
 
 app = responder.API()
 
 @app.route("/")
 async def index(request, response):
+    import logging
+
+    logging.info('================= Hello Logging =================')
+
     data = load_iris()
     response.text = str(data['DESCR'])
